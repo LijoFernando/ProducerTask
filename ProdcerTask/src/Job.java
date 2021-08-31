b748c3bad817cbed4a445ea905c966c8b325dc64
LijoFernando <rotricklijo@gmail.ocm>
2021-08-31 09:30:36 +0530

    Job Task

--- ProdcerTask/src/Job.java
+++ ProdcerTask/src/Job.java
@@ -1,74 +1,57 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Job {
-    public static int dayNumber;
+    public static ArrayList<String> arrayList= new ArrayList<>();
+    public static int arySize ;
+
     public static void main(String[] args) throws Exception {
-    Producer thread1 = new Producer();
-    Consumer thread2 = new Consumer();
+        Producer thread1 = new Producer();
+        Consumer thread2 = new Consumer();
         thread1.start();
         thread2.start();
 
-
     }
-
 }
+
 class Producer extends Thread{
-    FileInputStream fileInputStream = new FileInputStream("/home/rojo/IdeaProjects/ProdcerTask/src/T1File.txt");
-    Scanner scanLine = new Scanner(fileInputStream);
+    Reader reader = new FileReader("ProdcerTask/src/T1File.txt");
+   BufferedReader bfr = new BufferedReader(reader);
 
     Producer() throws FileNotFoundException {
-
     }
 
     @Override
     public synchronized void run() {
-        while (scanLine.hasNextLine()) {
-            int no = scanLine.nextInt();
-            Job.dayNumber = no;
-            no = 0;
-            System.out.println("Produced day: "+ Job.dayNumber);
-            try {
-                notify();
-                wait(300);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        String line = null ;
+        try {
+            while( bfr.readLine() != null) {
+                line = bfr.readLine();
+                System.out.println(line);
+                Job.arrayList.add(line);
+                notifyAll();
             }
         }
-
+        catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
     }
 }
+
 class Consumer extends Thread{
-    FileOutputStream fileOutputStream = new FileOutputStream("/home/rojo/IdeaProjects/ProdcerTask/src/T2File.txt");
-
-    Consumer() throws FileNotFoundException {
-    }
-
-
-
     @Override
     public synchronized void run() {
-        Boolean isTrue =true;
-        while (isTrue) {
-            if (Job.dayNumber != 0) {
+
+            for (int i =0; i < Job.arySize; i++) {
+                System.out.println(Job.arrayList.get(i));
+                notify();
                 try {
-                    fileOutputStream.write(Job.dayNumber);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("Consumed day: "+Job.dayNumber);
-                Job.dayNumber = 0;
-                try {
-                    notify();
-                    wait(300);
+                    wait(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-            else{
-                //isTrue = false;
-            }
-
-        }
     }
 }
